36b6ae40a62f4275cd96d84a76a71c3e5e8bee2d
pdevelter <47352461+pdevelter@users.noreply.github.com>
2019-05-08 10:35:20 +0200

    Add files via upload

--- FDIM miniproject/Miniprojet FDIM.py
+++ FDIM miniproject/Miniprojet FDIM.py
@@ -2,11 +2,15 @@ import numpy as np
 from scipy.linalg import expm
 import matplotlib.pyplot as plt
 
+plt.close('all')
+
+#those are the rates at which components fail (in minutes)
+
 jmphost_d=0.002
 lpad_d=0.002
 vpn_d=0.002
 cs_d=0.001
-repair=0.005
+repair=0.05
 
 #State 0: nothing down
 #1: vpn down
@@ -25,17 +29,23 @@ repair=0.005
 #14: jumphost+cs
 #15: cs
 
+#Q is the transition matrix corresponding to our markov chain
+
 Q=np.array([[-(vpn_d+lpad_d+jmphost_d+cs_d),vpn_d,0,0,0,0,0,0,0,lpad_d,0,0,0,jmphost_d,0,cs_d],[repair,-(repair+lpad_d+2*jmphost_d+cs_d),lpad_d,2*jmphost_d,cs_d,0,0,0,0,0,0,0,0,0,0,0],[0,repair,-(2*repair+4*jmphost_d+cs_d),0,0,2*jmphost_d,cs_d,0,0,repair,0,0,0,0,0,0],[0,repair,0,-(2*repair+lpad_d+2*cs_d),0,lpad_d,0,2*cs_d,0,0,0,0,0,repair,0,0],[0,repair,0,0,-(2*repair+lpad_d+2*jmphost_d),0,lpad_d,2*jmphost_d,0,0,0,0,0,0,0,repair],[0,0,repair,repair,0,-(3*repair+2*cs_d),0,0,2*cs_d,0,repair,0,0,0,0,0],[0,0,repair,0,repair,0,-(3*repair+4*jmphost_d),0,4*jmphost_d,0,0,repair,0,0,0,0],[0,0,0,repair,repair,0,0,-(3*repair+lpad_d),lpad_d,0,0,0,0,0,repair,0],[0,0,0,0,0,repair,repair,repair,-4*repair,0,0,0,repair,0,0,0],[repair,0,vpn_d,0,0,0,0,0,0,-(repair+vpn_d+2*jmphost_d+cs_d),2*jmphost_d,cs_d,0,0,0,0],[0,0,0,0,0,vpn_d,0,0,0,repair,-(2*repair+2*cs_d+vpn_d),0,2*cs_d,repair,0,0],[0,0,0,0,0,0,vpn_d,0,0,repair,0,-(2*repair+2*jmphost_d+vpn_d),2*jmphost_d,0,0,repair],[0,0,0,0,0,0,0,0,vpn_d,0,repair,repair,-(3*repair+vpn_d),0,repair,0],[repair,0,0,vpn_d,0,0,0,0,0,0,lpad_d,0,0,-(repair+vpn_d+lpad_d+2*cs_d),2*cs_d,0],[0,0,0,0,0,0,vpn_d,0,0,0,0,0,lpad_d,repair,-(2*repair+vpn_d+lpad_d),repair],[repair,0,0,0,vpn_d,0,0,0,0,0,0,lpad_d,0,0,jmphost_d,-(repair+jmphost_d+vpn_d+lpad_d)]])
+
+#this is the initial state vector at which we begin
 
 Ini=np.array([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
+#A(t) gives the sum of the probabilities of being in the states in the right vector
+
 def A(t):
-    return np.dot(np.dot(Ini,expm(Q*t)),[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-    
-print(A(1))    
-    
+    return np.dot(np.dot(Ini,expm(Q*t)),[0,0,0,0,1,0,1,1,1,0,0,1,1,0,1,1])
+        
 T=[i for i in range (0,1000,1)]
 A=[A(t) for t in T]
+
+#So those last lines will show the evolution of the probability of being in those states over time
 
 plt.plot(T,A)
 plt.show()
