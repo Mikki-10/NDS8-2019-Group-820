8209b18e77be1ae78bff598f8dca75220bcbe662
pdevelter <47352461+pdevelter@users.noreply.github.com>
2019-05-08 13:34:15 +0200

    Add files via upload

--- FDIM miniproject/Miniprojet FDIM.py
+++ FDIM miniproject/Miniprojet FDIM.py
@@ -33,6 +33,8 @@ repair=0.05
 
 Q=np.array([[-(vpn_d+ldap_d+jmphost_d+cs_d),vpn_d,0,0,0,0,0,0,0,ldap_d,0,0,0,jmphost_d,0,cs_d],[repair,-(repair+ldap_d+2*jmphost_d+cs_d),ldap_d,2*jmphost_d,cs_d,0,0,0,0,0,0,0,0,0,0,0],[0,repair,-(2*repair+4*jmphost_d+cs_d),0,0,2*jmphost_d,cs_d,0,0,repair,0,0,0,0,0,0],[0,repair,0,-(2*repair+ldap_d+2*cs_d),0,ldap_d,0,2*cs_d,0,0,0,0,0,repair,0,0],[0,repair,0,0,-(2*repair+ldap_d+2*jmphost_d),0,ldap_d,2*jmphost_d,0,0,0,0,0,0,0,repair],[0,0,repair,repair,0,-(3*repair+2*cs_d),0,0,2*cs_d,0,repair,0,0,0,0,0],[0,0,repair,0,repair,0,-(3*repair+4*jmphost_d),0,4*jmphost_d,0,0,repair,0,0,0,0],[0,0,0,repair,repair,0,0,-(3*repair+ldap_d),ldap_d,0,0,0,0,0,repair,0],[0,0,0,0,0,repair,repair,repair,-4*repair,0,0,0,repair,0,0,0],[repair,0,vpn_d,0,0,0,0,0,0,-(repair+vpn_d+2*jmphost_d+cs_d),2*jmphost_d,cs_d,0,0,0,0],[0,0,0,0,0,vpn_d,0,0,0,repair,-(2*repair+2*cs_d+vpn_d),0,2*cs_d,repair,0,0],[0,0,0,0,0,0,vpn_d,0,0,repair,0,-(2*repair+2*jmphost_d+vpn_d),2*jmphost_d,0,0,repair],[0,0,0,0,0,0,0,0,vpn_d,0,repair,repair,-(3*repair+vpn_d),0,repair,0],[repair,0,0,vpn_d,0,0,0,0,0,0,ldap_d,0,0,-(repair+vpn_d+ldap_d+2*cs_d),2*cs_d,0],[0,0,0,0,0,0,vpn_d,0,0,0,0,0,ldap_d,repair,-(2*repair+vpn_d+ldap_d),repair],[repair,0,0,0,vpn_d,0,0,0,0,0,0,ldap_d,0,0,jmphost_d,-(repair+jmphost_d+vpn_d+ldap_d)]])
 
+Qr=np.array([[-(vpn_d+ldap_d+jmphost_d+cs_d),vpn_d,0,0,0,0,0,0,0,ldap_d,0,0,0,jmphost_d,0,cs_d],[repair,-(repair+ldap_d+2*jmphost_d+cs_d),ldap_d,2*jmphost_d,cs_d,0,0,0,0,0,0,0,0,0,0,0],[0,repair,-(2*repair+4*jmphost_d+cs_d),0,0,2*jmphost_d,cs_d,0,0,repair,0,0,0,0,0,0],[0,repair,0,-(2*repair+ldap_d+2*cs_d),0,ldap_d,0,2*cs_d,0,0,0,0,0,repair,0,0],[0,0,0,0,-(repair+ldap_d+2*jmphost_d),0,ldap_d,2*jmphost_d,0,0,0,0,0,0,0,repair],[0,0,repair,repair,0,-(3*repair+2*cs_d),0,0,2*cs_d,0,repair,0,0,0,0,0],[0,0,0,0,0,0,-(4*jmphost_d),0,4*jmphost_d,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,-(ldap_d),ldap_d,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[repair,0,vpn_d,0,0,0,0,0,0,-(repair+vpn_d+2*jmphost_d+cs_d),2*jmphost_d,cs_d,0,0,0,0],[0,0,0,0,0,vpn_d,0,0,0,repair,-(2*repair+2*cs_d+vpn_d),0,2*cs_d,repair,0,0],[0,0,0,0,0,0,vpn_d,0,0,0,0,-(2*jmphost_d+vpn_d),2*jmphost_d,0,0,0],[0,0,0,0,0,0,0,0,vpn_d,0,0,0,-(vpn_d),0,0,0],[repair,0,0,vpn_d,0,0,0,0,0,0,ldap_d,0,0,-(repair+vpn_d+ldap_d+2*cs_d),2*cs_d,0],[0,0,0,0,0,0,vpn_d,0,0,0,0,0,ldap_d,0,-(vpn_d+ldap_d),0],[repair,0,0,0,vpn_d,0,0,0,0,0,0,ldap_d,0,0,jmphost_d,-(repair+jmphost_d+vpn_d+ldap_d)]])
+
 #this is the initial state vector at which we begin
 
 Ini=np.array([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
@@ -41,11 +43,17 @@ Ini=np.array([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
 def A(t):
     return np.dot(np.dot(Ini,expm(Q*t)),[0,0,0,0,1,0,1,1,1,0,0,1,1,0,1,1])
+    
+def R(t):
+    return np.dot(np.dot(Ini,expm(Qr*t)),[0,0,0,0,1,0,1,1,1,0,0,1,1,0,1,1])
+
         
-T=[i for i in range (0,1000,1)]
-A=[A(t) for t in T]
+A=[(1-A(t)) for t in T]
+R=[(1-R(t)) for t in T]
 
 #So those last lines will show the evolution of the probability of being in those states over time
 
-plt.plot(T,A)
+plt.plot(T,A,label='Availability')
+plt.plot(T,R,label='Relaiability')
+plt.legend(loc='best')
 plt.show()
