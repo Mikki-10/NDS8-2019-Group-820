8c67c4e55cea670a645e16a3e72f118a84209558
Mikki Sørensen <Mikki-10@hotmail.com>
2019-05-10 12:12:51 +0200

    A master peice of a login page

--- Project/Web 2FA/login.php
+++ Project/Web 2FA/login.php
@@ -26,20 +26,6 @@ else
 	show_login_form();
 }
 
-function show_login_form()
-{
-	?>
-	<form action="/login.php" method="post">
-	  	<fieldset>
-		    <input type="text" name="username" placeholder="Username">
-		    <input type="password" name="password">
-		    <input type="text" name="2fa" placeholder="2FA code">
-		    <input type="submit" name="login" value="Submit">
-	  	</fieldset>
-	</form>
-	<?php
-}
-
 
 function user_login($username, $password, $totp = "")
 {
@@ -107,9 +93,9 @@ function user_login($username, $password, $totp = "")
 						$url = $TOTP->make_totp_url(php_session_get_tofa(), $label, $issuer);
 
 						$QR_MAKER = new QR_MAKER();
-						echo $QR_MAKER->make_qr_code($url);	
+						$QR_CODE_URL = $QR_MAKER->make_qr_code($url);	
 
-						show_login_form();
+						show_login_form($QR_CODE_URL);
 					}
 				}
 				else
@@ -122,9 +108,9 @@ function user_login($username, $password, $totp = "")
 					$url = $TOTP->make_totp_url(php_session_get_tofa(), $label, $issuer);
 
 					$QR_MAKER = new QR_MAKER();
-					echo $QR_MAKER->make_qr_code($url);	
+					$QR_CODE_URL = $QR_MAKER->make_qr_code($url);	
 
-					show_login_form();
+					show_login_form($QR_CODE_URL);
 				}
 			}
 			else
@@ -140,35 +126,137 @@ function user_login($username, $password, $totp = "")
 				$url = $TOTP->make_totp_url($secret, $label, $issuer);
 
 				$QR_MAKER = new QR_MAKER();
-				echo $QR_MAKER->make_qr_code($url);
+				$qr_code_url = $QR_MAKER->make_qr_code($url);
 
-				show_login_form();
+				show_login_form($qr_code_url);
 			}
 		}
 	}
 }
 
 
+function show_login_form($qr_code_url = null)
+{
+	/*
+	<form action="/login.php" method="post">
+	  	<fieldset>
+		    <input type="text" name="username" placeholder="Username">
+		    <input type="password" name="password">
+		    <input type="text" name="2fa" placeholder="2FA code">
+		    <input type="submit" name="login" value="Submit">
+	  	</fieldset>
+	</form>
+	*/
+	?>
 
-/*
-$TOTP = new TOTP();
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>Login V4</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->	
+		<link rel="icon" type="image/png" href="login-html/images/icons/favicon.ico"/>
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/fonts/iconic/css/material-design-iconic-font.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/vendor/animate/animate.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="login-html/vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/vendor/animsition/css/animsition.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/vendor/select2/select2.min.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="login-html/vendor/daterangepicker/daterangepicker.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="login-html/css/util.css">
+		<link rel="stylesheet" type="text/css" href="login-html/css/main.css">
+	<!--===============================================================================================-->
+	<style>
+	img {
+	  display: block;
+	  margin-left: auto;
+	  margin-right: auto;
+	}
+	</style>
+	</head>
+	<body>
+		
+		<div class="limiter">
+			<div class="container-login100" style="background-image: url('login-html/images/bg-01.jpg');">
+				<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+					<form class="login100-form validate-form" action="/login.php" method="post">
+						<span class="login100-form-title p-b-49">
+							Login
+						</span>
 
-$secret = $TOTP->createSecret();
-$secret = "LHD2AHT2HGW55R5RVFERJQ75WWIVPWHY";
-my_debug_print($secret, __FILE__, __LINE__, "on");
+						<div>
+						<?php
+						echo "$qr_code_url";
+						?>
+						</div>
+						<br>
 
-$code = $TOTP->get_totp_code($secret);
-my_debug_print($code, __FILE__, __LINE__, "on");
+						<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+							<span class="label-input100">Username</span>
+							<input class="input100" type="text" name="username" placeholder="Type your username">
+							<span class="focus-input100" data-symbol="&#xf206;"></span>
+						</div>
 
+						<div class="wrap-input100 validate-input m-b-23" data-validate="Password is required">
+							<span class="label-input100">Password</span>
+							<input class="input100" type="password" name="password" placeholder="Type your password">
+							<span class="focus-input100" data-symbol="&#xf190;"></span>
+						</div>
 
-my_debug_print($TOTP->verify_totp($secret, $code), __FILE__, __LINE__, "on");
+						<div class="wrap-input100 m-b-23">
+							<span class="label-input100">2FA</span>
+							<input class="input100" type="text" name="2fa" placeholder="Type your 2FA code">
+							<span class="focus-input100" data-symbol="&#xf334;"></span>
+						</div>
+						
+						
+						<div class="container-login100-form-btn">
+							<div class="wrap-login100-form-btn">
+								<div class="login100-form-bgbtn"></div>
+								<button class="login100-form-btn" name="login">
+									Login
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		
 
-$label = "Username";
-$issuer = "Telenor.dk";
-$url = $TOTP->make_totp_url($secret, $label, $issuer);
+		<div id="dropDownSelect1"></div>
+		
+	<!--===============================================================================================-->
+		<script src="login-html/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="login-html/vendor/animsition/js/animsition.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="login-html/vendor/bootstrap/js/popper.js"></script>
+		<script src="login-html/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="login-html/vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+		<script src="login-html/vendor/daterangepicker/moment.min.js"></script>
+		<script src="login-html/vendor/daterangepicker/daterangepicker.js"></script>
+	<!--===============================================================================================-->
+		<script src="login-html/vendor/countdowntime/countdowntime.js"></script>
+	<!--===============================================================================================-->
+		<script src="login-html/js/main.js"></script>
 
-$QR_MAKER = new QR_MAKER();
-echo $QR_MAKER->make_qr_code($url);
-*/
+	</body>
+	</html>
+	<?php
+}
 
 ?>
